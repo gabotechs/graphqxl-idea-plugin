6d71e7f7fa1becb703416493b07dc69b2cc14b58
GitHub Action <action@github.com>
2022-08-31 19:59:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/gabotechs/graphqxlideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gabotechs/graphqxlideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gabotechs.graphqxlideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gabotechs.graphqxlideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
